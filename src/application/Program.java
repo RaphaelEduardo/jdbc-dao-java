127429e79766cda7fc63fda285c95ed9d2af3cc7
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-02 15:16:00 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,12 @@ public class Program {
 			System.out.println(s);	
 		}
 		
+		System.out.println("\n==== TEST 3: seller findAll ====");
+		list = sellerDao.findAll();
+		for (Seller s : list) {
+			System.out.println(s);	
+		}
+		
 		
 	}
 	
