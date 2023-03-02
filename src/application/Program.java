62e7a6bf5f69e41150f3b9376f4efcb951ccac6f
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-01 23:27:05 -0300

    Created Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 // Padrão de Projeto DAO (Data Access Object)
 public class Program {
@@ -18,5 +21,10 @@ public class Program {
 
 		Department d = new Department(1, "books");		
 		System.out.println(d);
+		
+		Seller s = new Seller(7 ,"Raphael", "raphael@gmail.com", new Date(), 3000.00, d);
+		System.out.println(s);
+	
 	}
+	
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,27 +2,27 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
-	
+public class Department implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
-	private int Id;
+	private Integer id;
 	private String name;
 
 	public Department() {
 	}
 
-	public Department(int id, String name) {
-		Id = id;
+	public Department(Integer id, String name) {
+		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
-		return Id;
+	public Integer getId() {
+		return id;
 	}
 
-	public void setId(int id) {
-		Id = id;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -37,8 +37,7 @@ public class Department implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + Id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -51,19 +50,17 @@ public class Department implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (Id != other.Id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!name.equals(other.name))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [Id=" + Id + ", name=" + name + "]";
+		return "Department [id=" + id + ", name=" + name + "]";
 	}
 
 }
